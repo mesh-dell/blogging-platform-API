f410a1e49acc4f67e3e521d58e9e6a1fcd998827
mesh-dell <mwaimonicas@gmail.com>
2025-12-05 00:51:32 +0300

    feat: add BlogPost and BlogPostRequest models for blog post management

---
+++ internal/blog/model.go
@@ -0,0 +1,20 @@
+package model
+
+import "time"
+
+type BlogPost struct {
+	Id        int       `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Category  string    `json:"category"`
+	Tags      []string  `json:"tags"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type BlogPostRequest struct {
+	Title    string   `json:"title" binding:"required"`
+	Content  string   `json:"content" binding:"required"`
+	Category string   `json:"category" binding:"required"`
+	Tags     []string `json:"tags"`
+}
