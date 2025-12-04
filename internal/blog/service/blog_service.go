4a8e450bd4585c5429acb9d02abe154596fe5a48
mesh-dell <mwaimonicas@gmail.com>
2025-12-05 00:51:08 +0300

    feat: implement BlogPostService for managing blog posts

---
+++ internal/blog/service/blog_service.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	model "github.com/mesh-dell/blogging-platform-API/internal/blog"
+	"github.com/mesh-dell/blogging-platform-API/internal/blog/repository"
+)
+
+type BlogPostService struct {
+	repo repository.IBlogPostsRepository
+}
+
+func NewBlogPostService(r repository.IBlogPostsRepository) *BlogPostService {
+	return &BlogPostService{repo: r}
+}
+
+func (s *BlogPostService) CreateBlog(req model.BlogPostRequest) (model.BlogPost, error) {
+	return s.repo.CreateBlog(req)
+}
+
+func (s *BlogPostService) GetBlogById(id int) (model.BlogPost, error) {
+	return s.repo.GetBlogById(id)
+}
+
+func (s *BlogPostService) GetBlogs() ([]model.BlogPost, error) {
+	return s.repo.GetBlogs()
+}
+
+func (s *BlogPostService) UpdateBlog(id int, req model.BlogPostRequest) (model.BlogPost, error) {
+	return s.repo.UpdateBlog(id, req)
+}
+
+func (s *BlogPostService) DeleteBlog(id int) error {
+	return s.repo.DeleteBlog(id)
+}
